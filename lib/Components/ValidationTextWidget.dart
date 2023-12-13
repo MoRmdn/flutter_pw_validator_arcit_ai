c71897f220a126101fa05c3b3459886ce0721bc7
Mohamed Ramadan <60930725+MoRmdn@users.noreply.github.com>
2023-12-13 08:58:31 +0200

    Update ValidationTextWidget.dart

--- lib/Components/ValidationTextWidget.dart
+++ lib/Components/ValidationTextWidget.dart
@@ -6,18 +6,27 @@ class ValidationTextWidget extends StatelessWidget {
   final Color color;
   final String text;
   final int? value;
+  final bool? showSuccessIcon;
+  final bool? showFailureIcon;
+  final Widget? successIcon, failureIcon;
 
-  ValidationTextWidget(
-      {required this.color, required this.text, required this.value});
+  ValidationTextWidget({
+    required this.color,
+    required this.text,
+    required this.value,
+    this.showSuccessIcon,
+    this.showFailureIcon,
+    this.successIcon,
+    this.failureIcon,
+  });
 
   @override
   Widget build(BuildContext context) {
     return new Row(
       children: [
         new Container(
-          width: SizeConfig.width! * 0.03,
-          height: SizeConfig.width! * 0.03,
           child: new CircleAvatar(
+            radius: 3,
             backgroundColor: color,
           ),
         ),
@@ -28,7 +37,14 @@ class ValidationTextWidget extends StatelessWidget {
             style:
                 new TextStyle(fontSize: SizeConfig.width! * 0.04, color: color),
           ),
-        )
+        ),
+
+        /// If showSuccessIcon is true then show successIcon else show failureIcon
+        /// If showSuccessIcon and showFailureIcon is null then show nothing
+
+        if (showSuccessIcon == true && successIcon != null) successIcon!,
+
+        if (showFailureIcon == true && failureIcon != null) failureIcon!,
       ],
     );
   }
